328e1e759fe7dd7258b26cb62891be731812feee
Zoe Faltibà <zoefaltiba@gmail.com>
2025-01-24 15:36:14 +0100

    add WitnessOrd::Ignored

--- cli/src/args.rs
+++ cli/src/args.rs
@@ -131,7 +131,7 @@ impl RgbArgs {
             let resolver = self.resolver()?;
             let from_height = self.from_height.unwrap_or(1);
             eprint!("Updating witness information starting from height {from_height} ... ");
-            let res = stock.update_witnesses(resolver, from_height)?;
+            let res = stock.update_witnesses(resolver, from_height, vec![])?;
             eprint!("{} transactions were checked and updated", res.succeeded);
             if res.failed.is_empty() {
                 eprintln!();
